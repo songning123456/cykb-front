f3f20edfad80bec0fdede12624b2776a5a841b4d
songning <1457065857@qq.com>
2020-03-07 15:04:44 +0800

    管理按钮

--- components/TopBar.vue
+++ components/TopBar.vue
@@ -1,7 +1,19 @@
 <template>
     <view class="top-bar cu-bar bg-white">
         <view class="action">
-            <text v-if="title === '收藏'" class="cuIcon-more"></text>
+            <text v-if="title === '收藏'" class="cuIcon-more" @tap="showModal" data-target="menuModal"></text>
+            <view class="cu-modal" :class="modalName==='menuModal'?'show':''" @tap="hideModal">
+                <view class="cu-dialog">
+                    <view class="cu-list menu text-left">
+                        <view class="cu-item" v-for="(item, index) in modalList" :key="index">
+                            <view class="content" hover-class="text-red" hover-stay-time="1000">
+                                <text class="text-red" :class="'cuIcon-' + item.key"></text>
+                                <text class="text-black">{{item.value}}</text>
+                            </view>
+                        </view>
+                    </view>
+                </view>
+            </view>
             <switch v-if="title === '排行'" class='switch-sex'
                     @change="changeSex" :class="sex?'checked':''" :checked="sex"></switch>
         </view>
@@ -25,22 +37,54 @@
         },
         data() {
             return {
-                sex: true
+                sex: true,
+                modalName: null,
+                modalList: [
+                    {key: 'delete', value: '书籍管理'},
+                    {key: 'list', value: '收藏模式'},
+                    {key: 'down', value: '缓存管理'}
+                ]
             }
         },
         methods: {
             changeSex(e) {
                 this.sex = e.detail.value;
+            },
+            showModal(e) {
+                this.modalName = e.currentTarget.dataset.target
+            },
+            hideModal(e) {
+                this.modalName = null
             }
         }
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
     .top-bar {
         top: 85px;
         position: fixed;
         width: 100%;
+
+        .cu-modal {
+            transform: unset;
+
+            .cu-dialog {
+                width: 240rpx;
+                bottom: 480rpx;
+                right: 240rpx;
+
+                .cu-item {
+                    min-height: 60rpx;
+
+                    .content {
+                        width: 100%;
+                        font-size: 24rpx;
+                        line-height: 2.6em;
+                    }
+                }
+            }
+        }
     }
 
 </style>
