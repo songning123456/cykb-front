f051cb45c17460f87f0a8442d3030d266d9cb8fc
songning <1457065857@qq.com>
2020-03-08 18:07:37 +0800

    mine 页面初步完成

--- pages/home/children/Mine.vue
+++ pages/home/children/Mine.vue
@@ -1,10 +1,41 @@
 <template>
-    <view class="mine full-width"></view>
+    <view class="mine full-width">
+        <view class="cu-list menu">
+            <view class="cu-item arrow" v-for="(item, index) in displayInfo" :key="index">
+                <view class="content">
+                    <text class="text-black" :class="'cuIcon-' + item.icon"></text>
+                    <text class="text-black">{{item.title}}</text>
+                </view>
+            </view>
+        </view>
+    </view>
 </template>
 
 <script>
     export default {
-        name: "Mine"
+        name: "Mine",
+        data() {
+            return {
+                displayInfo: [
+                    {
+                        icon: 'appreciate',
+                        title: '请给我好评'
+                    },
+                    {
+                        icon: 'share',
+                        title: '分享给朋友'
+                    },
+                    {
+                        icon: 'comment',
+                        title: '意见反馈'
+                    },
+                    {
+                        icon: 'settings',
+                        title: '设置'
+                    }
+                ]
+            }
+        }
     }
 </script>
 
