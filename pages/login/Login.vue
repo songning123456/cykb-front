d4e6c3e81a1690c0dad19e62972528c0d41ce3e0
songning <songning@kedacom.com>
2020-03-09 17:14:15 +0800

    登录页初步完成

--- pages/login/Login.vue
+++ pages/login/Login.vue
@@ -1,8 +1,8 @@
 <template>
     <view class="index-style flex solid-bottom padding justify-center">
         <view class="flex solid-bottom padding align-center">
-            <button class="cu-btn block bg-blue margin-tb-sm lg" @tap="login">
-                <!--            <text class="cuIcon-loading2 cuIconfont-spin"></text>-->
+            <button class="cu-btn block bg-blue margin-tb-sm lg" open-type="getUserInfo" @getuserinfo="loginIntoWechat"
+                    withCredentials="true">
                 登录
             </button>
         </view>
@@ -13,11 +13,60 @@
 <script>
     export default {
         name: "Login",
+        mounted() {
+            uni.getStorage({
+                key: 'userInfo',
+                success: response1 => {
+                    uni.navigateTo({
+                        url: '/pages/home/Home?user=' + response1
+                    });
+                },
+                fail: reject1 => {
+                    debugger;
+                    // doNothing...
+                }
+            });
+        },
         methods: {
-            login() {
-                uni.navigateTo({
-                    url: '/pages/home/Home'
-                })
+            loginIntoWechat() {
+                debugger;
+                uni.login({
+                    success: response2 => {
+                        debugger;
+                        uni.showLoading({
+                            title: '登陆中'
+                        });
+                        // 获取用户信息
+                        uni.getUserInfo({
+                            provider: 'weixin',
+                            success: response3 => {
+                                debugger;
+                                uni.hideLoading();
+                                uni.setStorage({
+                                    key: 'userInfo',
+                                    data: response3
+                                });
+                            },
+                            fail: reject3 => {
+                                debugger;
+                                uni.hideLoading();
+                                uni.showToast({
+                                    title: '获取用户信息失败',
+                                    duration: 1000,
+                                    icon: 'none'
+                                });
+                            }
+                        });
+                    },
+                    fail: response2 => {
+                        debugger;
+                        uni.showToast({
+                            title: '登录失败',
+                            duration: 1000,
+                            icon: 'none'
+                        });
+                    }
+                });
             }
         }
     }
