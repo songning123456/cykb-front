205f8bc14d436e26de2f6f4e58d8889bdac27053
songning <songning@kedacom.com>
2020-03-10 18:53:53 +0800

    mine 初步优化

--- pages/home/children/Mine.vue
+++ pages/home/children/Mine.vue
@@ -1,5 +1,25 @@
 <template>
     <view class="mine full-width">
+        <view class="cu-card case no-card">
+            <view class="cu-item shadow">
+                <view class="image">
+                    <image src="https://ossweb-img.qq.com/images/lol/web201310/skin/big10006.jpg"
+                           mode="widthFix"></image>
+                </view>
+                <view class="cu-list menu-avatar">
+                    <view class="cu-item">
+                        <view class="cu-avatar round lg"
+                              style="background-image:url(https://ossweb-img.qq.com/images/lol/web201310/skin/big10006.jpg);"></view>
+                        <view class="content flex-sub">
+                            <view class="text-gray text-sm flex justify-between">
+                                十天前
+                            </view>
+                            <view class="text-grey">正义天使 凯尔</view>
+                        </view>
+                    </view>
+                </view>
+            </view>
+        </view>
         <view class="cu-list menu">
             <view class="cu-item arrow" hover-class='hover-class-style' hover-stay-time='1200'
                   v-for="(item, index) in displayInfo" :key="index" @tap="tapBtn(item.icon)">
