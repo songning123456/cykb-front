c19db027c205187d4be53e09fe32deef39d7ade9
songning <songning@kedacom.com>
2020-03-10 13:50:47 +0800

    登录页初步完成

--- pages/login/Login.vue
+++ pages/login/Login.vue
@@ -1,7 +1,7 @@
 <template>
     <view class="index-style flex solid-bottom padding justify-center">
         <view class="flex solid-bottom padding align-center">
-            <button class="cu-btn block bg-blue margin-tb-sm lg" open-type="getUserInfo" @getuserinfo="loginIntoWechat"
+            <button class="cu-btn block bg-blue margin-tb-sm lg" open-type="getUserInfo" @getuserinfo="loginWx"
                     withCredentials="true">
                 登录
             </button>
@@ -11,14 +11,21 @@
 </template>
 
 <script>
+    import request from '../../util/request';
+
     export default {
         name: "Login",
+        data() {
+            return {
+                userInfo: {}
+            };
+        },
         mounted() {
             uni.getStorage({
                 key: 'userInfo',
                 success: response1 => {
                     uni.navigateTo({
-                        url: '/pages/home/Home?user=' + response1
+                        url: '/pages/home/Home?userInfo=' + response1
                     });
                 },
                 fail: reject1 => {
@@ -28,11 +35,9 @@
             });
         },
         methods: {
-            loginIntoWechat() {
-                debugger;
+            loginWx() {
                 uni.login({
                     success: response2 => {
-                        debugger;
                         uni.showLoading({
                             title: '登陆中'
                         });
@@ -41,14 +46,41 @@
                             provider: 'weixin',
                             success: response3 => {
                                 debugger;
-                                uni.hideLoading();
-                                uni.setStorage({
-                                    key: 'userInfo',
-                                    data: response3
-                                });
+                                let params = {
+                                    condition: {
+                                        code: response2.code,
+                                        avatar: response3.userInfo.avatarUrl,
+                                        nickName: response3.userInfo.nickName
+                                    }
+                                };
+                                request.post('/users/weixin/getUsersInfo', params).then(data => {
+                                    uni.hideLoading();
+                                    if (data.status === 200) {
+                                        this.userInfo = data.data[0];
+                                        uni.setStorage({
+                                            key: 'userInfo',
+                                            data: this.userInfo
+                                        });
+                                        uni.navigateTo({
+                                            url: '/pages/home/Home?userInfo=' + this.userInfo
+                                        });
+                                    } else {
+                                        uni.showToast({
+                                            title: '获取用户信息失败',
+                                            duration: 1000,
+                                            icon: 'none'
+                                        });
+                                    }
+                                }).catch(e => {
+                                    uni.hideLoading();
+                                    uni.showToast({
+                                        title: '获取用户信息失败',
+                                        duration: 1000,
+                                        icon: 'none'
+                                    });
+                                })
                             },
                             fail: reject3 => {
-                                debugger;
                                 uni.hideLoading();
                                 uni.showToast({
                                     title: '获取用户信息失败',
@@ -59,7 +91,6 @@
                         });
                     },
                     fail: response2 => {
-                        debugger;
                         uni.showToast({
                             title: '登录失败',
                             duration: 1000,
